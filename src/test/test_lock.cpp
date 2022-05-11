0384d60319a2b930f38d2c3623ff6998829f9467
whpark <whitedevil75@naver.com>
2022-05-11 14:39:10 +0900

    shared_mutex

--- src/test/test_lock.cpp
+++ src/test/test_lock.cpp
@@ -18,67 +18,71 @@ namespace gtl::test::recursive_shared_mutex {
 		gtl::xSimpleLog log;
 
 		//std::vector<std::unique_ptr<std::pair<std::thread::id, int> > > vecP;
+		gtl::TConcurrentUPtrVector<std::pair<std::thread::id, int>> vecP;
 
-		//int const maxP = 1'000;
-		//vecP.reserve(maxP);
+		int const maxP = 1'000;
+		vecP.reserve(maxP);
 
-		//std::atomic<int> nReader = 0;
-		//std::atomic<int> nWriter = 0;
-		//int nMaxReader = 0;
-		//int nMaxWriter = 0;
+		std::atomic<int> nReader = 0;
+		std::atomic<int> nWriter = 0;
+		int nMaxReader = 0;
+		int nMaxWriter = 0;
+		size_t const nThread{10};
 
-		//auto Producer = [&]() {
-		//	auto id = std::this_thread::get_id();
-		//	for (int i = 0; i < maxP; i++) {
-		//		std::shared_lock lock6(vecP);
+		std::latch latch(nThread);
+		auto Producer = [&]() {
+			latch.count_down();
+			auto id = std::this_thread::get_id();
+			for (int i = 0; i < maxP; i++) {
+				//std::shared_lock lock6(vecP);
 
-		//		std::scoped_lock lock(vecP);
-		//		nWriter++;
-		//		if (nMaxWriter < nWriter) nMaxWriter = nWriter;
-		//		vecP.push_back(std::make_unique<std::pair<std::thread::id, int>>(id, i));
-		//		nWriter--;
+				std::scoped_lock lock(vecP);
+				nWriter++;
+				if (nMaxWriter < nWriter) nMaxWriter = nWriter;
+				vecP.push_back(std::make_unique<std::pair<std::thread::id, int>>(id, i));
+				nWriter--;
 
-		//		std::shared_lock lock2(vecP);
-		//		std::shared_lock lock3(vecP);
-		//		std::unique_lock lock4(vecP);
-		//		std::unique_lock lock5(vecP);
+				std::shared_lock lock2(vecP);
+				std::shared_lock lock3(vecP);
+				std::unique_lock lock4(vecP);
+				std::unique_lock lock5(vecP);
 
-		//		std::this_thread::sleep_for(1ms);
-		//	}
-		//};
-		//auto Adder = [&]() {
-		//	do {
-		//		double dSum = 0;
-		//		{
-		//			std::shared_lock lock(vecP);
-		//			nReader++;
-		//			if (nMaxReader < nReader) nMaxReader = nReader;
+				//std::this_thread::sleep_for(1ms);
+			}
+		};
+		auto Adder = [&]() {
+			latch.arrive_and_wait();
+			do {
+				double dSum = 0;
+				{
+					std::shared_lock lock(vecP);
+					nReader++;
+					if (nMaxReader < nReader) nMaxReader = nReader;
 
-		//			if (nWriter)
-		//				throw std::runtime_error("failed");
+					if (nWriter)
+						throw std::runtime_error("failed");
 
-		//			for (int i = 0; i < vecP.size(); i++) {
-		//				dSum += vecP[i]->second;
-		//			}
-		//			nReader--;
+					for (int i = 0; i < vecP.size(); i++) {
+						dSum += vecP[i].second;
+					}
+					nReader--;
 
-		//			std::this_thread::sleep_for(1ms);
-		//		}
-		//	} while (vecP.size() < maxP);
-		//};
+					//std::this_thread::sleep_for(1ms);
+				}
+			} while (vecP.size() < maxP);
+		};
 
-		//std::vector<std::unique_ptr<std::thread> > threads;
-		//for (int i = 0; i < 10; i++) {
-		//	if (i < 3)
-		//		threads.push_back(std::make_unique<std::thread>(Producer));
-		//	//else
-		//	//	threads.push_back(make_unique<thread>(Adder));
-		//}
+		std::vector<std::unique_ptr<std::thread> > threads;
+		size_t iThread{};
+		for (; iThread < 3; iThread++)
+			threads.push_back(std::make_unique<std::thread>(Producer));
+		for (; iThread < nThread; iThread++)
+			threads.push_back(std::make_unique<std::thread>(Adder));
 
-		//for (auto& rThread : threads) {
-		//	if (rThread)
-		//		rThread->join();
-		//}
+		for (auto& rThread : threads) {
+			if (rThread)
+				rThread->join();
+		}
 
 	}
 }
