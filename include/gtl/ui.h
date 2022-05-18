65d806151dcf1e7dc452fe641d40d5f4782fc234
whpark <whitedevil75@naver.com>
2022-05-18 12:04:59 +0900

    ui header

---
+++ include/gtl/ui.h
@@ -0,0 +1,29 @@
+#pragma once
+
+//////////////////////////////////////////////////////////////////////
+//
+// ui	
+//
+// PWH
+//
+// 2021.05.12. from Mocha
+//
+//////////////////////////////////////////////////////////////////////
+
+#include <cstdint>
+#include <cmath>
+#include <numbers>
+#include "gtl/concepts.h"
+
+#include "gtl/ui/ui_predefine.h"
+#include "gtl/ui/ui_unit.h"
+#include "gtl/ui/ui_prop.h"
+#include "gtl/ui/ui_prop_value_color.h"
+#include "gtl/ui/ui_prop_value_etc.h"
+#include "gtl/ui/ui_widget.h"
+
+namespace gtl::ui {
+	inline namespace literals {
+
+	};
+};	// namespace gtl
