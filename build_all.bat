8005c0939cd7dbd70c08041180b327124f647923
whpark <whitedevil75@naver.com>
2023-04-05 15:03:49 +0900

    build x64 only

--- build_all.bat
+++ build_all.bat
@@ -10,16 +10,16 @@ echo +  x64 - Release
 echo ------------------
 msbuild gtl.sln /p:Platform=x64 /p:Configuration=Release -m
 if %ERRORLEVEL% neq 0  goto :error_end
-echo ------------------
-echo +  win32 - Debug
-echo ------------------
-msbuild gtl.sln /p:Platform=x86 /p:Configuration=Debug -m
-if %ERRORLEVEL% neq 0  goto :error_end
-echo ------------------
-echo +  win32 - Release
-echo ------------------
-msbuild gtl.sln /p:Platform=x86 /p:Configuration=Release -m
-if %ERRORLEVEL% neq 0  goto :error_end
+rem echo ------------------
+rem echo +  win32 - Debug
+rem echo ------------------
+rem msbuild gtl.sln /p:Platform=x86 /p:Configuration=Debug -m
+rem if %ERRORLEVEL% neq 0  goto :error_end
+rem echo ------------------
+rem echo +  win32 - Release
+rem echo ------------------
+rem msbuild gtl.sln /p:Platform=x86 /p:Configuration=Release -m
+rem if %ERRORLEVEL% neq 0  goto :error_end
 
 goto :end
 
