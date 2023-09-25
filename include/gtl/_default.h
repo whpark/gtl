21db040fa6cc1e26a58260aff0f0cd1fdf44f589
whpark <whitedevil75@naver.com>
2023-09-25 12:01:58 +0900

    enumerate (https://www.reedbeta.com/blog/python-like-enumerate-in-cpp17/)

--- include/gtl/_default.h
+++ include/gtl/_default.h
@@ -182,6 +182,34 @@ namespace std {
 #endif
 
 
+#ifdef __cpp_lib_ranges_enumerate
+#else
+// https://www.reedbeta.com/blog/python-like-enumerate-in-cpp17/
+namespace std {
+	template <typename T,
+		typename TIter = decltype(std::begin(std::declval<T>())),
+		typename = decltype(std::end(std::declval<T>()))>
+	constexpr auto enumerate(T && iterable)
+	{
+		struct iterator
+		{
+			size_t i;
+			TIter iter;
+			bool operator != (const iterator & other) const { return iter != other.iter; }
+			void operator ++ () { ++i; ++iter; }
+			auto operator * () const { return std::tie(i, *iter); }
+		};
+		struct iterable_wrapper
+		{
+			T iterable;
+			auto begin() { return iterator{ 0, std::begin(iterable) }; }
+			auto end() { return iterator{ 0, std::end(iterable) }; }
+		};
+		return iterable_wrapper{ std::forward<T>(iterable) };
+	}
+}
+#endif
+
 // NUM_ARGS(...) macro arg 갯수
 #define NUM_ARGS_64(                            _64,_63,_62,_61,_60,_59,_58,_57,_56,_55,_54,_53,_52,_51,_50,_49,_48,_47,_46,_45,_44,_43,_42,_41,_40,_39,_38,_37,_36,_35,_34,_33,_32,_31,_30,_29,_28,_27,_26,_25,_24,_23,_22,_21,_20,_19,_18,_17,_16,_15,_14,_13,_12,_11,_10,_9,_8,_7,_6,_5,_4,_3,_2,_1, N, ...) N
 #define NUM_ARGS(...) NUM_ARGS_64( __VA_ARGS__ , 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
