cfa2b3fab5fc0f2e27993d056adf608d8681c0f6
whpark <whitedevil75@naver.com>
2023-10-01 20:26:11 +0900

    .

--- src/test/test_misc.cpp
+++ src/test/test_misc.cpp
@@ -56,7 +56,9 @@ doubleVar                = 3.1415926535897932384626433832795028 ; pi
 			TLazyProfile<char8_t> profile;
 			auto r = std::filesystem::current_path();
 			std::istringstream stream(strLazyProfile);
+			auto t0 = std::chrono::steady_clock::now();
 			profile.Load(stream);
+			auto t1 = std::chrono::steady_clock::now();
 			profile[u8"Test"].SetItemValue(u8"path", 2);
 			profile[u8"Test"].SetItemValue(u8"LogTranslatorEvents", false);
 			//profile[u8"Test"].SetItemValue(u8"String", u8"\" 바바바바바");	// error.
@@ -64,7 +66,18 @@ doubleVar                = 3.1415926535897932384626433832795028 ; pi
 
 			profile[u8"key having punctuation()- and spaces"].SetItemValue(u8"path", u8"");
 
+			auto t2 = std::chrono::steady_clock::now();
+
 			profile.Save("ini/lazy_profileU8.ini");
+
+			auto t3 = std::chrono::steady_clock::now();
+
+			auto GetDuration = [](auto t0, auto t1) {
+				return std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0);
+			};
+			fmt::print("Load profile : {}\n", GetDuration(t0, t1));
+			fmt::print("Setting 4 items to profile : {}\n", GetDuration(t1, t2));
+			fmt::print("Save profile : {}\n", GetDuration(t2, t3));
 		}
 
 		{
