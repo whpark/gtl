0c74fb6a3812b81e6f72e37a9269935771bd6038
whpark <whitedevil75@naver.com>
2023-10-23 13:43:57 +0900

    vector_map -> container_map

--- include/gtl/container_map.h
+++ include/gtl/container_map.h
@@ -4,7 +4,7 @@
 //
 //			GTL : (G)reen (T)ea (L)atte
 //
-// 2023.09.27. a simple map class based on std::vector and std::pair
+// 2023-10-23. a simple map class using std container and std::pair (originally, gtl::TVectorMap)
 //
 // PWH
 //
@@ -14,15 +14,17 @@
 #pragma once
 
 #include <vector>
+#include <deque>
+#include <list>
 #include <algorithm>
 
 namespace gtl {
 
-	template <class TKey, class T, class TKeyEq = std::equal_to<TKey>/*, class TAlloc = std::allocator<std::pair<TKey const, T>>*/>
-	class TVectorMap : protected std::vector<std::pair<TKey const, T>/*, TAlloc*/> {
+	template <template <typename ...TArg> typename TContainer, class TKey, class T, class TKeyEq = std::equal_to<TKey>, class TAlloc = std::allocator<std::pair<TKey const, T>> >
+	class TContainerMap : protected TContainer<std::pair<TKey const, T>, TAlloc> {
 	public:
-		using this_t = TVectorMap;
-		using base_t = std::vector<std::pair<TKey const, T>/*, TAlloc*/>;
+		using this_t = TContainerMap;
+		using base_t = TContainer<std::pair<TKey const, T>/*, TAlloc*/>;
 
 	public:
 		using base_t::base_t;
@@ -33,6 +35,7 @@ namespace gtl {
 	public:
 		base_t& base() noexcept { return *this; }
 		base_t const& base() const noexcept { return *this; }
+		~TContainerMap() = default;
 
 		using base_t::begin;
 		using base_t::end;
@@ -45,14 +48,26 @@ namespace gtl {
 
 		using base_t::front;
 		using base_t::back;
-		using base_t::data;
+		//using base_t::data;
+		constexpr decltype(auto) data() const noexcept requires requires (base_t container) { container.data(); } {
+			return base_t::data();
+		}
 
 		using base_t::empty;
 		using base_t::size;
 		using base_t::max_size;
-		using base_t::reserve;
-		using base_t::capacity;
-		using base_t::shrink_to_fit;
+		//using base_t::reserve;
+		constexpr void reserve(size_t size) requires requires (base_t container) { container.reserve(size); } {
+			return base_t::reserve(size);
+		}
+		//using base_t::capacity;
+		constexpr auto capacity() const noexcept requires requires (base_t container) { container.capacity(); } {
+			return base_t::capacity();
+		}
+		//using base_t::shrink_to_fit;
+		constexpr void shrink_to_fit() requires requires (base_t container) { container.shrink_to_fit(); } {
+			base_t::shrink_to_fit();
+		}
 
 		using base_t::clear;
 		using base_t::swap;
@@ -66,7 +81,7 @@ namespace gtl {
 			if (auto iter = find(key); iter != end()) {
 				return iter->second;
 			}
-			throw std::out_of_range("TVectorMap::operator[]");
+			throw std::out_of_range("TDequeMap::operator[]");
 		}
 
 		template < typename TKey2 > requires (requires(TKey2 key2) { TKey{} = key2; })
@@ -83,7 +98,8 @@ namespace gtl {
 				return iter;
 			}
 			base_t::emplace_back(key, T{});
-			return end()-1;
+			auto iter = end();
+			return --iter;
 		}
 
 		template < typename TKey2 > requires (requires(TKey2 key2) { TKey{} = key2; })
