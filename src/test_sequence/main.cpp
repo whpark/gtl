efc49eb4cac1ab6078259e793f1a71a19d9db250
whpark <whitedevil75@naver.com>
2024-08-08 16:13:01 +0900

    .

--- src/test_sequence/main.cpp
+++ src/test_sequence/main.cpp
@@ -5,12 +5,6 @@
 #include "fmt/format.h"
 #include "fmt/xchar.h"
 
-#ifdef _DEBUG
-#	pragma comment(lib, "catch2d.lib")
-#else
-#	pragma comment(lib, "catch2.lib")
-#endif
-
 int main(int argc, char** argv) {
 	gtl::SetCurrentPath_ProjectFolder();
 	fmt::print("Current Folder : {}\n\n", std::filesystem::current_path().string().c_str());
