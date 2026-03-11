d964e3a51a348a85912b62c33aa7d61b8abe4077
whpark <whitedevil75@naver.com>
2026-03-11 20:30:02 +0900

    .

--- etc/vcpkg/install-vcpkg.bat
+++ etc/vcpkg/install-vcpkg.bat
@@ -57,8 +57,6 @@ vcpkg install --triplet=x64-windows --recurse gtest catch2 benchmark boost flux 
 rem deleted : maddy, websocketpp,
 if %errorlevel% neq 0 set FAILED=%FAILED% x64-windows,
 
-call move_opencv4.bat
-
 rem skip x86
 goto end
 
@@ -71,12 +69,27 @@ echo installing x86-windows...
 vcpkg install --triplet=x86-windows --recurse gtest catch2 benchmark boost flux cppzmq grpc 7zip bzip2 libarchive fmt imgui spdlog scnlib ms-gsl magic-enum libenvpp winreg tinyxml2 nlohmann-json tomlplusplus libxml2 libxmlmm libmodbus openssl opencv[core] wxwidgets[debug-support,fonts]
 if %errorlevel% neq 0 set FAILED=%FAILED% x86-windows,
 
+rem ================================================
+rem end
 :end
 
-set VCPKG_FOLDER=
+rem PopDirectory
 if %PopDirectory% neq 0 popd & set PopDirectory= & set CallBootStrap=
 
-call
+rem if vcpkg/
+if Exist "%VCPKG_FOLDER%\installed\x64-windows\include\opencv4\opencv2" (
+	echo !====
+	echo !  move include/opencv4/opencv2 to include/opencv2/
+	echo !====
+	call move_opencv4.bat
+) else (
+	echo !====
+	echo !  skip move include/opencv4/opencv2 to include/opencv2/
+	echo !====
+)
+
+rem cleanup
+set VCPKG_FOLDER=
 
 if "%FAILED%" neq "" (
 	echo
